b3a82423349ee2141c71dd584b9b793d41913a3e
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2026-02-11 10:42:00 +0530

    Sum of two numbers

---
+++ Python For Data Engineering/Practice/BasicIntroduction/sum.py
@@ -0,0 +1,5 @@
+#   Take two numbers and print the sum of both.
+num1 = int(input("Please Enter 1st Number: "))
+num2 = int(input("Please Enter 2nd Number: "))
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum}")
