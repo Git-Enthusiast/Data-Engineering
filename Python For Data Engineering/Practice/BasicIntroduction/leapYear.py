3318ea0eb67c2b960266a7fd7e97bc560e54a526
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2026-02-11 10:37:16 +0530

    feat: add script to determine if a given year is a leap year

---
+++ Python For Data Engineering/Practice/BasicIntroduction/leapYear.py
@@ -0,0 +1,11 @@
+#   Input a year and find whether it is a leap year or not.
+
+year = int(input("Please Enter a year."))
+if(year%400 == 0):
+    print(f"{year} is a leap year.")
+elif(year%100 == 0):
+    print(f"{year} is not a leap year.")
+elif(year%4 == 0):
+    print(f"{year} is a leap year.")
+else :
+    print(f"{year} is not a leap year.")
