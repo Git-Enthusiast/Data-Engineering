74752e4a9042d85ccabe853443c52ab5a14f4d82
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2026-02-14 15:36:05 +0530

    feat: Add `enumerate` function examples for strings, lists, and tuples.

--- Python For Data Engineering/Basics-7/Enumerate.py
+++ Python For Data Engineering/Basics-7/Enumerate.py
@@ -2,3 +2,24 @@ myString = "Hello World"
 
 for i, j in enumerate(myString ,start=1):
     print(i, j)
+
+
+myList = ["apple", "banana", "cherry"]
+
+for index, fruit in enumerate(myList):
+    print(index, fruit)
+
+# Output:
+# 0 apple
+# 1 banana
+# 2 cherry
+
+myTuple = ("apple", "banana", "cherry")
+
+for index, fruit in enumerate(myTuple):
+    print(index, fruit)
+
+# Output:
+# 0 apple
+# 1 banana
+# 2 cherry
