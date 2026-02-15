85af516eee1fba03140ee3edf09cc76a8195e18a
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2026-02-15 07:06:07 +0530

    feat: Add script to prompt for a name and print a personalized greeting.

---
+++ Python For Data Engineering/Practice/Basics Programs/print_greeting.py
@@ -0,0 +1,6 @@
+#   Take name as input and print a greeting message for that particular name.
+name = input("Please Enter your name:")
+def greet(name):
+    print(f"Hi, {name} How are You?")
+
+greet(name)
