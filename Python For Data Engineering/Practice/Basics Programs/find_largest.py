43d952ff13fe2f6d014db5654703be5e53404a70
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2026-02-16 14:49:28 +0530

    feat: Largest among two

---
+++ Python For Data Engineering/Practice/Basics Programs/find_largest.py
@@ -0,0 +1,8 @@
+#   Take 2 numbers as input and print the largest number.
+num1 = float(input("Please Enter 1st number: "))
+num2 = float(input("Please Enter 2nd number: "))
+
+if num1>num2:
+    print(f"{num1} is the largest among {num1} and {num2}")
+else:
+     print(f"{num2} is the largest among {num1} and {num2}")
