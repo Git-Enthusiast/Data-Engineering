431d98992a256996da3ac671c09e89d8558b09e8
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2026-02-19 18:24:41 +0530

    feat: Add program to calculate the area of a rectangle.

---
+++ Python For Data Engineering/Practice/Loops and Conditionals/calc_ar_rectangle.py
@@ -0,0 +1,10 @@
+#   Program to calculate area of rectangle
+def calculate_area_rectangle(length, width):
+    return length * width
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = calculate_area_rectangle(length, width)
+
+print("Area of Rectangle =", area)
